b6b790635e749be0cc5e139f1236fee11270859e
lukesdm <2824165+lukesdm@users.noreply.github.com>
2020-12-02 20:33:01 +0000

    Day 1 - implemented for example - tested & passing

--- FSharp/Day1.fs
+++ FSharp/Day1.fs
@@ -1,12 +1,26 @@
-﻿module Day1
+﻿/// https://adventofcode.com/2020/day/1
+module Day1
 
 open Expecto
 
-// TODO: Implement
-let part1 = 12
-let part2 = 24
+// "find the two entries that sum to 2020 and then multiply those two numbers together"
+let part1 input =
+    Seq.allPairs input input
+    |> Seq.filter (fun (l, r) -> l + r = 2020)
+    |> Seq.map (fun (l, r) -> l * r)
+    |> Seq.head
 
-let solve = (part1, part2)
+// TODO:
+let part2 _ = 24
+
+let solve input = (part1 input, part2 input)
 
 let tests =
-    testList "" [ test "A simple test" { Expect.equal 1 1 "Not equal" } ]
+    testList
+        "Part 1"
+        [ test "example 1" {
+            let input = [ 1721; 979; 366; 299; 675; 1456 ]
+
+            let (part1_actual, _) = solve input
+            Expect.equal part1_actual 514579 ""
+          } ]
