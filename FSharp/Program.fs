83c18397582f84d08258b7aa2ae1de5056b126af
lukesdm <2824165+lukesdm@users.noreply.github.com>
2020-12-02 15:50:32 +0000

    Add expecto

--- FSharp/Program.fs
+++ FSharp/Program.fs
@@ -1,28 +1,37 @@
+open Expecto
 open System
 
- // TODO: Implement
+// TODO: Implement
 let d1p1 = 12
 let d1p2 = 24
 
 let solve_day1 = (d1p1, d1p2)
 
-let parseArgs (args: string[]) : Option<string> =
+let tests =
+    [ ("1", testList "" [ test "A simple test" { Expect.equal 1 1 "Not equal" } ]) ]
+    |> Map.ofList
+
+let parseArgs (args: string []): Option<string> =
     if args.Length = 1 then Some(args.[0]) else None
 
 [<EntryPoint>]
 let main argv =
-    let day = 
+    let day =
         match (parseArgs argv) with
-        | Some(day) -> day
+        | Some (day) -> day
         | None ->
             printfn "Usage: expected a single argument of the form {DAY}, e.g. '1'"
             exit 1
-    
-    let result = 
+
+    let testsPassed = runTestsWithCLIArgs [] [||] tests.[day]
+    if testsPassed <> 0 then exit 1
+
+    let result =
         match day with
         | "1" -> solve_day1
         | _ ->
             printfn $"Not implemented for day {day}"
             exit 1
+
     printfn $"Result for day {day}: {result}"
     0
