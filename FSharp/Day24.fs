642634e62ba6b0c3cf3e51f2a685292b2e02756c
lukesdm <2824165+lukesdm@users.noreply.github.com>
2020-12-24 12:31:14 +0000

    Day 24 WIP - i1 ok, but i2 not.

--- FSharp/Day24.fs
+++ FSharp/Day24.fs
@@ -2,6 +2,7 @@
 module Day24
 
 open Expecto
+open System
 open System.Text.RegularExpressions
 
 /// "directions are given in your list, respectively, as e, se, sw, w, nw, and ne"
@@ -57,6 +58,79 @@ let part1 (input: string): int =
     |> Seq.countBy (fun coord -> coord)
     |> Seq.filter (fun (_, count) -> count % 2 = 1)
     |> Seq.length
+
+// ***PART 2***
+
+type TileQR = int * int
+type Tiles = Set<TileQR>
+
+/// Calculates the initial state, as the set of black tiles
+let initialState (input: string): Tiles =
+    // Similar to part 1, get black tiles by determining those with an odd number of flips
+    let flipsQR =
+        input.Replace("\r\n", "\n").Split('\n')
+        |> Seq.map parseLine
+        |> Seq.map qrCoords
+
+    flipsQR
+    |> Seq.countBy (fun coord -> coord)
+    |> Seq.filter (fun (_, count) -> count % 2 = 1)
+    |> Seq.map (fun (coord, _) -> coord)
+    |> Set.ofSeq
+
+let countNeighbors blackTiles (q, r) =
+    seq {
+        for dq in -1 .. 1 do
+            for dr in -1 .. 1 do
+                if (dq, dr) <> (0, 0) then yield (dq, dr)
+    }
+    |> Seq.filter (fun (dq, dr) -> Set.contains (q + dq, r + dr) blackTiles)
+    |> Seq.length
+
+let getBounds (blackTiles: Tiles): TileQR * TileQR =
+    let init =
+        ((Int32.MaxValue, Int32.MaxValue), (Int32.MinValue, Int32.MinValue))
+
+    blackTiles
+    |> Seq.fold
+        (fun currBounds tile ->
+            let ((minQ, minR), (maxQ, maxR)) = currBounds
+            let (cQ, cR) = tile
+            ((min cQ minQ, min cR minR), (max cQ maxQ, max cR maxR)))
+        init
+
+let next (blackTiles: Tiles): Tiles =
+    let isBlack = fun cell -> Seq.contains cell blackTiles
+
+    let ((qMin, rMin), (qMax, rMax)) = getBounds blackTiles
+
+    // Cell space: bounding box with 1-tile padding
+    let tiles =
+        seq {
+            for q in qMin - 1 .. qMax + 1 do
+                for r in rMin - 1 .. rMax + 1 do
+                    yield (q, r)
+        }
+
+    seq {
+        for tile in tiles do
+            let nc = countNeighbors blackTiles tile
+
+            match nc with
+            | 1 when isBlack tile -> yield tile
+            | 2 -> yield tile
+            | _ -> ()
+    }
+    |> Set.ofSeq
+
+let gen i0 =
+    i0
+    |> Seq.unfold
+        (fun state ->
+            let nxt = next state
+            Some(nxt, nxt))
+
+// ***...***
 
 let solve input = part1 input
 
@@ -140,6 +214,27 @@ let tests =
               let count_expected = 10
 
               let count_actual = part1 input
+
+              Expect.equal count_actual count_expected ""
+          }
+          test "Part 2 - Have correct number of tiles after day 1" {
+              let input = example
+              let i0 = initialState input
+
+              let count_expected = 15
+
+              let count_actual = Seq.length (next i0)
+
+              Expect.equal count_actual count_expected ""
+          }
+          ftest "Part 2 - Have correct number of tiles after day 2" {
+              let input = example
+              let i0 = initialState input
+
+              let count_expected = 12
+
+              let results = i0 |> gen |> Seq.take(8) |> Array.ofSeq
+              let count_actual = Seq.length results.[1]
 
               Expect.equal count_actual count_expected ""
           } ]
